1040aadee782b0e1bbecc03f0374fa5548206996
Ewan RIVA <eriva@290070u.local>
2024-12-11 15:24:32 +0100

    initial commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,21 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Anonyme::index');
+$routes->get('/anonyme', 'Anonyme::index');
+$routes->post('/anonyme/seConnecter', 'Anonyme::seConnecter');
+$routes->get('/visiteur', 'Visiteur::index');
+$routes->get('/visiteur/mesFiches', 'Visiteur::mesFiches');
+$routes->get('/visiteur/voirMaFiche/(:num)', 'Visiteur::voirMaFiche/$1'); 
+$routes->get('/visiteur/modMaFiche/(:num)', 'Visiteur::modMaFiche/$1'); 
+$routes->get('/visiteur/signeMaFiche/(:num)', 'Visiteur::signeMaFiche/$1'); 
+$routes->get('/visiteur/seDeconnecter', 'Visiteur::SeDeconnecter');
+$routes->post('/visiteur/updateForfait/(:num)', 'Visiteur::updateForfait/$1');
+$routes->post('/visiteur/ajouteUneLigneDeFrais/(:num)', 'Visiteur::ajouteUneLigneDeFrais/$1');
+$routes->get('/visiteur/deleteUneLigneDeFrais/(:num)/(:num)', 'Visiteur::deleteUneLigneDeFrais/$1/$2');
+$routes->get('/anonyme/login', 'Anonyme::login');
---
+++ app/Controllers/Anonyme.php
@@ -0,0 +1,62 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use \App\Models\Authentif;
+
+/**
+ * Accès à l'application par défaut pour tout utilisateur non authentifié.
+ * Gestion du formulaire de connexion et sa soumission
+ */
+class Anonyme extends BaseController
+{
+	/**
+	 * Détecte si l'utilisateur est authentifié et envoie la view adaptée à son profil ou 
+	 * le formulaire de connexion s'il n'est pas authentifié.
+	 */
+	public function index()
+	{
+		$authentif = new Authentif();
+		if ($authentif->estVisiteur()) 
+		{
+			return redirect()->to('/visiteur');
+		}
+		else
+		{
+			return $this->login ();
+		}
+	}
+
+	/**
+	 * Envoi du formulaire de connexion, lequel peut incorporer un message d'erreur
+	 *
+	 */
+	public function login($errMsg = null)
+	{
+		$data = array('erreur'=>$errMsg);
+		return view('v_connexion', $data);
+	}
+
+	/**
+	 * Traite la soumission du formulaire de connexion afin d'authentifier l'utilisateur
+	 * 
+	 */
+	public function seConnecter () 
+	{	// TODO : conrôler que l'obtention des données postées ne rend pas d'erreurs 
+
+		$login = $this->request->getPost('login');
+		$mdp = $this->request->getPost('mdp');
+		
+		$authentif = new Authentif();
+		$authUser = $authentif->authentifier($login, $mdp);
+
+		if(empty($authUser))
+		{
+			return $this->login ('Login ou mot de passe incorrect');
+		}
+		else
+		{
+			$authentif->connecter($authUser);
+			return $this->index();
+		}
+	}
+}
